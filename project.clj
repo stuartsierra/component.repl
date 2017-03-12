1eaf02c950c0f87677626a187ea5fdb111fb22ca
Stuart Sierra <mail@stuartsierra.com>
2017-03-12 19:56:46 -0400

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/component.repl "0.2.0"
+(defproject com.stuartsierra/component.repl "0.2.1-SNAPSHOT"
   :description "Interactive development utilities for the Component framework"
   :url "https://github.com/stuartsierra/component.repl"
   :license {:name "MIT license"
